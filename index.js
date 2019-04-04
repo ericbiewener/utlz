2c647c1b5ea0314e1eab2151df37b879ca6d661e
Eric Biewener <ebiewener@gmail.com>
2019-04-04 08:55:03 -0700

    add readDirFilesSync

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const fs = require('fs')
+const path = require('path')
 
-exports.sleep = function sleep(ms = 100) {
+function sleep(ms = 0) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-exports.isFile = function isFile(file) {
+function isFile(file) {
   try {
     return fs.statSync(file).isFile()
   } catch (e) {
     if (e.code !== 'ENOENT') throw e // File might exist, but something else went wrong (e.g. permissions error)
     return false
   }
+}
+
+function readDirFilesSync(dir) {
+  const items = []
+  
+  for (const name of fs.readdirSync(dir)) {
+    const filepath = path.join(dir, name)
+    if (!isFile(filepath) || name === '.DS_Store') continue
+    items.push({ filename: name, filepath })
+  }
+
+  return items
+}
+
+module.exports = {
+  sleep,
+  isFile,
+  readDirFilesSync,
 }
