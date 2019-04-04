7916dd8c1cd05eb6f29fcd849530371f9d59b1b2
Eric Biewener <ebiewener@gmail.com>
2019-04-04 08:36:40 -0700

    Add isFile

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 exports.sleep = function sleep(ms = 100) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+exports.isFile = function isFile(file) {
+  try {
+    return fs.statSync(file).isFile()
+  } catch (e) {
+    if (e.code !== 'ENOENT') throw e // File might exist, but something else went wrong (e.g. permissions error)
+    return false
+  }
+}
